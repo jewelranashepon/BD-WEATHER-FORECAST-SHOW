a3abf4e64b96d1bac5442ef9a12d43dd7e8cb735
Jewel Rana <jewelranashepon@gmail.com>
2025-07-02 19:00:38 +0600

    Update removing files

--- app/dashboard/upper-air-analyzer/page.tsx
+++
@@ -1,9 +0,0 @@
-import MeteorologicalAnalysis from "@/components/MeteorologicalAnalysis";
-
-export default function Page() {
-  return (
-    <div>
-      <MeteorologicalAnalysis />
-    </div>
-  );
-}
